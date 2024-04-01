fef306e43a6abdb442e4bdfb9118409d2a937d6f
andresAc99 <80556603+andresAc99@users.noreply.github.com>
2024-04-01 11:04:26 -0500

    main

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,14 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Andres Amancio.</h2>;
+  const three = <h3 className="big-heading">a consulting data analyst and social researcher.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I'm a data analyst specialized in the area of Business Intelligence (and occasionally in data science). {' '}
+        <a href="https://www.kaggle.com/andresamancio" target="_blank" rel="noreferrer">
+          My Data Science portfolio.
         </a>
         .
       </p>
@@ -78,10 +76,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.youtube.com/@DataforBreakfast"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my class!
     </a>
   );
 
